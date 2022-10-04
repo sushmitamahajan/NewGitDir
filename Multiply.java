0bcccbaf560f4145bba2309c5a3172456aad6fb7
Vedant Mahajan <mahajan@git>
2022-10-04 19:43:44 +0530

    multiply changes

--- Multiply.java
+++ Multiply.java
@@ -6,6 +6,6 @@ public class Multiply
   public static void main(String[] args) {
     int a = 10;
     int b = 20;
-    System.out.println(a*b);
+    System.out.println("Multiplication is ",a*b);
   }
 }
